e48708f1ae709588b4de86c795e9e9c48d8329b4
Rupak Sarkar <112826831+Rupak100@users.noreply.github.com>
2024-08-21 15:48:31 +0530

    Update cron.js

--- cron.js
+++ cron.js
@@ -1,20 +1,20 @@
-// const cron = require("cron");
-// const https = require("https");
+const cron = require("cron");
+const https = require("https");
 
-// const URL = "https://edispensary-management.onrender.com";
+const URL = "https://edispensary-management.onrender.com";
 
-// const job = new cron.CronJob("*/14 * * * *", function () {
-//   https
-//     .get(URL, (res) => {
-//       if (res.statusCode === 200) {
-//         console.log("GET request sent successfully");
-//       } else {
-//         console.log("GET request failed", res.statusCode);
-//       }
-//     })
-//     .on("error", (e) => {
-//       console.error("Error while sending request", e);
-//     });
-// });
+const job = new cron.CronJob("*/14 * * * *", function () {
+  https
+    .get(URL, (res) => {
+      if (res.statusCode === 200) {
+        console.log("GET request sent successfully");
+      } else {
+        console.log("GET request failed", res.statusCode);
+      }
+    })
+    .on("error", (e) => {
+      console.error("Error while sending request", e);
+    });
+});
 
-// job.start(); // Start the cron job
+job.start(); // Start the cron job
